95a0ef0e416a142a60137e3161c0136e55524552
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-04-17 17:17:20 +0900

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-
+    
+    
     // task2-1
     printf("好きな食べ物は蕎麦です。\n"); // why: 安いから
     printf("好きな教科は情報です。\n"); // why: 楽しいから
@@ -9,7 +10,8 @@ int main(void){
     printf("\n");
     printf("よろしくお願いします。");
     return 0;
-
+    
+    
     /*
     // task2-2
     int x = 5;
@@ -20,6 +22,24 @@ int main(void){
     printf("z= %d\n", z);
     return 0;
     */
-
+    
+    /*
+    // task2-3
+    int x, y;
+    double z, w;
+    scanf("%d %d", &x, &y);
+    scanf("%lf %lf", &z, &w);
+    printf("x+y=%d\n", x+y);
+    printf("x-y=%d\n", x-y);
+    printf("x*y=%d\n", x*y);
+    printf("x/y=%d\n", x/y);
+    printf("x%y=%d\n", x%y);
+    printf("z+w=%lf\n", z+w);
+    printf("z-w=%lf\n", z-w);
+    printf("z*w=%lf\n", z*w);
+    printf("z/w=%lf\n", z/w);
+    printf("x+z=%lf\n", x+z);
+    printf("x+z*y=%lf\n", x+z*y);
+    */
     
 }
