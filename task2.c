cd5632b960b825b7300779e51043cefa14938cd3
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-04-17 15:32:36 +0900

    Create task2.c

---
+++ task2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+
+    // task2-1
+    printf("好きな食べ物は蕎麦です。\n"); // why: 安いから
+    printf("好きな教科は情報です。\n"); // why: 楽しいから
+    printf("\n");
+    printf("\n");
+    printf("よろしくお願いします。");
+    return 0;
+
+    /*
+    // task2-2
+    int x = 5;
+    int y = 7;
+    int z = 9;
+    printf("x= %d", x);
+    printf("y= %d", y);
+    printf("z= %d", z);
+    return 0;
+    */
+
+    
+}
