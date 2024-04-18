24934b54845f8788e56714b4b250d5390a440bbf
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-04-18 09:00:39 +0900

    Update report2.py

--- report2.py
+++ report2.py
@@ -1,8 +1,16 @@
 # basic
+
+# read num x3
 a = float(input())
 b = float(input())
 c = float(input())
+
+# print sum
 print("sum:", a + b + c)
+
+# print average
 print("ave:", (a + b + c) / 3)
 
 # extra
+
+# ......??????
