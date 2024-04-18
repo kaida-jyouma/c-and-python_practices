85caf43738e02a7d098993b43cbfcdaadee30be8
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-04-18 15:34:52 +0900

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,11 +1,11 @@
-x, y, z = [c for c in input().split()] # 半角スペース区切りで入力を受け取る
+x, y, z = [c for c in input().split()] # read number from console (format: n1 n2 n3)
 print("xの型は", type(x))
 print("yの型は", type(y))
 print("zの型は", type(z))
-x1, y1, z1 = [int(c) for c in [x, y, z]] # x, y, zをintに型変換
-ans1 = x1 / y1 / z1 # x1/y1/z1を変数ans1に格納
-ans2 = x1 // y1 // z1 # x1//y1//z1を変数ans2に格納
+x1, y1, z1 = [int(c) for c in [x, y, z]] # convert x,y,z to int
+ans1 = x1 / y1 / z1 # update ans1 with x1/y1/z1
+ans2 = x1 // y1 // z1 # update ans2 with x1//y1//z1
 print("x1/y1/z1 =", ans1)
 print("x1//y1//z1 =", ans2)
-print("typeof 'x1/y1/z1' =", type(ans1)) # ans1の型を表示
-print("typeof 'x1//y1//z1 ='", type(ans2)) # ans2の型を表示
+print("typeof 'x1/y1/z1' =", type(ans1)) # print type of 'ans1'
+print("typeof 'x1//y1//z1 ='", type(ans2)) # print type of 'ans2'
