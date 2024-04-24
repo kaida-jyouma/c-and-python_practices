b9f718c15734f4c321fcd5a8f49d543ca3dc3d8e
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-04-24 16:20:40 +0900

    add comment in task3.py

--- task3.py
+++ task3.py
@@ -1,13 +1,15 @@
 # 3task3.py
-rice = 1700
-bread = 200
-money = int(input())
+rice = 1700 # define rice's price
+bread = 200 # define bread's price
+money = int(input()) # receive money
 
-if money >= rice:
-    money -= rice
-for i in range(4):
-    if money >= bread:
-        money -= bread
-    else:
-        break
+if money >= rice: # if we can buy rice
+    money -= rice # buy one
+
+for i in range(4): # bread_max: 4pcs
+    if money >= bread: # if we can buy bread
+        money -= bread # buy bread
+    else: # if we cannot buy bread
+        break # quit purchase
+
 print(money)
