c41d074cf5830fc51025f9cff0b2636b531824fc
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-04-26 09:39:48 +0900

    Update task3.c

--- section3/task3.c
+++ section3/task3.c
@@ -6,13 +6,18 @@ int main(void){
     
     double a; // define "a" with type "double"
     scanf("%lf", &a); // receive number and input it to "a"
+    double stnum = 1001;
     if (a == 0 || a == 0.1 || a == 1){
         printf("%d", 0);
     }else if (a == 0.2 || a == 3){
-        printf("%lf", 3.14); // my student number is 2610240070......
-    }
+        printf("%lf", stnum);
+    }else{
+		printf("%lf", 3.14); 
+	}
     return 0;
-    
+        
+}
+
 
     
     // 3task22.c
