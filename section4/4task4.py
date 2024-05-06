7eb34cadd7ea330c12c11e382221a1ec988c0d7c
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-06 22:54:07 +0900

    add los function in 4task4.py

--- section4/4task4.py
+++ section4/4task4.py
@@ -1,4 +1,5 @@
 def loc(a, b=None, c=None, degAB=None):
+    # you have to install sympy before...
     import sympy as sp
     import math as mt
     if [a, b, c, degAB].count(None) > 1:
@@ -18,11 +19,47 @@ def loc(a, b=None, c=None, degAB=None):
             degAB = mt.radians(degAB)
 
             # calculate
-            b = sp.Symbol("b")
+            b = sp.Symbol("b") # replace b(None) to symbol "b"(char)
             fom = a**2 + b**2 - c**2 - 2*a*b * sp.cos(degAB)
-            return sp.solve(fom, b) # need to check
+            return sp.solve(fom, b) # need to check -> ok
 
         elif  degAB == None:
             fom = (a**2 + b**2 - c**2) / (2*a*b)
             return mt.degrees(sp.acos(fom))
+        
+        else:
+            # maybe unnecessary......
+            return -2
 
+def los(a=None, degA=None, r_2r=None):
+    from math import sin, asin, radians
+    if [a, degA, r_2r].count(None) > 1:
+        return -1
+    else:
+        if r_2r == None:
+            return a / sin(radians(degA))
+
+        elif degA == None:
+            return asin(a / r_2r)
+
+        elif a == None:
+            return r_2r * sin(radians(degA))
+
+        else:
+            return -2
+
+# q1
+print(loc(7, 8, None, 120))
+
+# q2
+from math import sqrt as sq
+print(loc(6, sq(3), sq(21)))
+
+# q3
+print(sorted(loc(sq(3), None, 1, 30))[0])
+
+# q4
+print(sorted(loc(sq(3), None, 1, 30))[1])
+
+# q5
+print(los(None, 60, los(sq(6), 45)))
