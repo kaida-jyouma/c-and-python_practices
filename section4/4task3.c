ae5c6194b945ab0bb1546de2380fa1d0e9a3e2a9
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-06 19:48:52 +0900

    create 4task3.c

---
+++ section4/4task3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calcSum(int req){
+    int ret = 0;
+    if (req > 500){
+        ret += 13000 + 10 * (req - 500);
+    }else if (req > 100){
+        ret += 5000 + 20 * (req - 100);
+    }else{
+        ret += 50 * req;
+    }
+    return ret;
+}
+
+int main(void){
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    int total = calcSum(a) + calcSum(b) + calcSum(c);
+    printf("%d", total);
+    return 0;
+}
