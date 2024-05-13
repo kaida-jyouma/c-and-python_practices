7d504d72a4d9b1d244c46164ce8e70933d9b6db7
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-13 16:58:00 +0900

    edit task5.c

--- section5/task5.c
+++ section5/task5.c
@@ -47,7 +47,7 @@ int main(){
     // printf("%f\n", gcd(3289, 5083));
     int n, m;
     scanf("%d %d\n", &n, &m);
-    printf("&lf", lcm(n, m));
+    printf("%lf", lcm(n, m));
     return 0;
 }
 // */
