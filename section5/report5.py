d5ef269d7cfe04ad068d3a379ae7ef2a88de0c61
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-13 17:45:30 +0900

    add extra report submission in report5.py

--- section5/report5.py
+++ section5/report5.py
@@ -13,3 +13,16 @@ while True:
 
 
 # extra
+
+# detect now time (1/100sec)
+
+from time import time
+ct = 0
+while True:
+    dt = input("detect now time (input 2-digit number): "))
+    t = str(time()).split(".")[1][:2]
+    if dt == t or ct > 10:
+        print("Success!")
+        break
+    else:
+        ct += 1
