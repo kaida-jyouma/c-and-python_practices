2b7da22c8369b11b6c26131bd447dceabec1d654
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-13 16:38:20 +0900

    modify task5.c and task5.py

--- section5/task5.c
+++ section5/task5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-
 // 5task2
+// /*
 double factorial(int n){
     double ans = 1;
     for (int i=n;i>0;i--) ans *= i;
@@ -18,3 +18,32 @@ int main(){
     printf("%f\n", comb(m, n));
     return 0;
 }
+// */
+
+// 5task42
+// /*
+double gcd(int n, int m){
+    // convert x > y
+    if (n > m){
+        int x = n;
+        int y = m;
+    }else if (n < m){
+        int x = m;
+        int y = n;
+    }
+    if (y == 0){
+        return x;
+    }else{
+        return gcd(y, x % y);
+    }
+}
+
+double lcm(int n, int m){
+    
+}
+
+int main(){
+    printf("%d\n", gcd(6, 3));
+    return 0;
+}
+// */
--- section5/task5.py
+++ section5/task5.py
@@ -1,6 +1,16 @@
+# 5task3
+
 def leibniz_pi(n):
     return sum([( (-1) ** (c - 1) ) / ( 2*c - 1 ) for c in range(1, n + 1)]) * 4
 
 print(leibniz_pi(5))
 print(leibniz_pi(20))
 print(leibniz_pi(100))
+
+
+
+# 5task41
+
+from math import lcm
+n, m = map(int, input().split())
+print(lcm(n, m))
