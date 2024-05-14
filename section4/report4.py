018616645401932a347fc883cd44af6012d64b6f
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-14 14:30:01 +0900

    edit report4.py

--- section4/report4.py
+++ section4/report4.py
@@ -7,59 +7,8 @@ from math import radians as rad
 
 import sympy as sp
 
-def loc(a, b=None, c=None, degAB=None):
-    # you have to install sympy before...
-    import sympy as sp
-    import math as mt
-    if [a, b, c, degAB].count(None) > 1:
-        return -1
-
-    else:
-        if c == None:
-            # deg => rad
-            degAB = mt.radians(degAB)
-
-            # calculate
-            sq = a**2 + b**2 - 2*a*b * sp.cos(degAB)
-
-            return mt.sqrt(sq)
-
-        elif b == None:
-            # deg => rad
-            degAB = mt.radians(degAB)
-
-            # calculate
-            b = sp.Symbol("b") # replace b(None) to symbol "b"(char)
-            fom = a**2 + b**2 - c**2 - 2*a*b * sp.cos(degAB)
-            return sp.solve(fom, b) # need to check -> ok
-
-        elif  degAB == None:
-            fom = (a**2 + b**2 - c**2) / (2*a*b)
-            return mt.degrees(sp.acos(fom))
-        
-        else:
-            # maybe unnecessary... -> necessary !
-            return -2
-            
-def los(a=None, degA=None, r_2r=None):
-    from math import sin, asin, radians
-    if [a, degA, r_2r].count(None) > 1:
-        return -1
-        
-    else:
-        if r_2r == None:
-            return a / sin(radians(degA))
-
-        elif degA == None:
-            return asin(a / r_2r)
-
-        elif a == None:
-            return r_2r * sin(radians(degA))
-
-        else:
-            return -2
-
-a = sp.Symbol("a")
+# a = sp.Symbol("a")
+a = 100
 
 pd = (a * sin(rad(20))) / sin(rad(80))
 ab = (a * sin(rad(80))) / sin(rad(50))
@@ -70,8 +19,14 @@ ce = ab - ae
 bc = (ab * sin(rad(80))) / sin(rad(50))
 dc = bc - a
 
-de = loc(dc, ce, degAB=50)
+de = sp.sqrt((dc ** 2 + ce ** 2) - 2 * dc * ce * cos(rad(50)))
 
-x = asin((pd * sin(rad(100))) / de)
+print(de)
+
+d = (pd * sin(rad(100))) / de
+
+print(d)
+
+x = deg(asin(d))
 
 print(x)
