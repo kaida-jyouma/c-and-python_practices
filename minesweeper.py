06fc2af55b406f1ad0a6fc08b7e34f70f16938a5
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-16 11:59:37 +0900

    add function "detect_mine" in minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,44 @@
 # play with terminal
 
 # under construction......
+def detect_mine(x, y, fld, rowmax, colmax, mine="o", grd="x"):
+	# row = x, col = y
+	
+	# get situation around target cell
+	if x == 0:
+		if y == 0:
+			nearby = [fld[0][1], fld[1][0], fld[1][1]]
+		elif y == colmax - 1:
+			nearby = [fld[0][-2], fld[1][-2], fld[1][-1]]
+		else:
+			nearby = [fld[0][y-1], fld[0][y+1], fld[1][y-1], fld[1][y], fld[1][y+1]]
+
+	elif x == rowmax - 1:
+		if y == 0:
+			nearby = [fld[-2][0], fld[-2][1], fld[-1][1]]
+		elif y == colmax - 1:
+			nearby = [fld[-2][-2], fld[-2][-1], fld[-1][-2]]
+		else:
+			nearby = [fld[-2][y-1], fld[-2][y], fld[-2][y+1], fld[-1][y-1], fld[-1][y+1]]
+
+	else:
+		if y == 0:
+			nearby = [fld[x-1][y], fld[x-1][y+1], fld[x][y+1], fld[x+1][y], fld[x+1][y+1]]
+		elif y == colmax - 1:
+			nearby = [fld[x-1][y-1], fld[x-1][y], fld[x][y-1], fld[x+1][y-1], fld[x+1][y]]
+		else:
+			nearby = [fld[x-1][y-1], fld[x-1][y], fld[x-1][y+1], fld[x][y-1], fld[x][y+1], fld[x+1][y-1], fld[x+1][y], fld[x+1][y+1]]
+
+
+	# judge bomb number around target cell
+	if fld[x][y] == mine:
+		return mine
+	
+	elif fld[x][y] == grd:
+		density = nearby.count(mine)
+		return density
+
+
 def makefield(row, col):
     from math import floor
     from random import randint as ri
@@ -15,13 +53,16 @@ def makefield(row, col):
 
     # make field and set bomb ratio as 15%
     for i in range(row):
-        fl = ["x" if ri(0, 99) < 15 else "o" for c in range(col)]
+        fl = ["o" if ri(0, 99) < 15 else "x" for c in range(col)]
         bnum += fl.count("x") # count bomb
         fld.append(fl) 
 
-    return {bnum: fld}
+    return {"bnum": bnum, "fld": fld}
 
+def game():
 
+    fld_row = 14
+    fld_col = 18
 
-
-
+    fld = makefield(fld_row, fld_col)["fld"]
+    
