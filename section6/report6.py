c1017832127897cf8e9018a281dfa027e297b37d
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-22 16:34:39 +0900

    edit report6.py

--- section6/report6.py
+++ section6/report6.py
@@ -1,3 +1,4 @@
+# decreasing model
 def cat_nxt(n, m):
 	return n - 0.04*(n - 1000) + 0.04*(m - 3000)
 
@@ -9,9 +10,14 @@ def grs_nxt(m, k):
 
 n, m, k = map(int, input().split())
 
+print(n, m, k)
 for i in range(1, 361):
 	n = cat_nxt(n, m)
 	m = rab_nxt(m, m, k)
 	k = grs_nxt(m, k)
 	if i % 10 == 0:
 		print(" , ".join([str(n), str(m), str(k)]))
+	if n < 0 or m < 0 or k < 0:
+		print(" , ".join([str(n), str(m), str(k)]))
+		print(i, "extinct break")
+		break
