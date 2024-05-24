593dab367bd4519a6bccad53107be49358d4cd29
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-24 14:28:12 +0900

    Update report6.py

--- section6/report6.py
+++ section6/report6.py
@@ -12,12 +12,15 @@ n, m, k = map(int, input().split())
 
 print(n, m, k)
 for i in range(1, 361):
-	n = cat_nxt(n, m)
-	m = rab_nxt(m, m, k)
-	k = grs_nxt(m, k)
+	n1 = cat_nxt(n, m)
+	m1 = rab_nxt(m, m, k)
+	k1 = grs_nxt(m, k)
 	if i % 10 == 0:
-		print(" , ".join([str(n), str(m), str(k)]))
+		print(" , ".join([str(n1), str(m1), str(k1)]))
 	if n < 0 or m < 0 or k < 0:
-		print(" , ".join([str(n), str(m), str(k)]))
+		print(" , ".join([str(n1), str(m1), str(k1)]))
 		print(i, "extinct break")
 		break
+	n = n1
+	m = m1
+	k = k1
