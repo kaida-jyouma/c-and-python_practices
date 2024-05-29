cb53a4c6781d90637e672979adbc3b1821a08d2f
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-29 18:10:30 +0900

    add 7task4 in task7.c

--- section7/task7.c
+++ section7/task7.c
@@ -56,3 +56,22 @@ int main(){
 	}
 
 }
+
+// 7task4.c
+
+extern double v_t(double n);
+
+double v_s(double n){
+	if ( n == 0 ) return 2.0;
+	else return sqrt(v_s(n - 1) * v_t(n - 1));
+}
+
+double v_t(double n){
+	if ( n == 0 ) return 4.0;
+	else return (2 * (v_s(n) / v_t(n - 1))) / (1 + v_s(n) / v_t(n - 1)) * v_t(n - 1);
+}
+
+int main(){
+	for (int i=0;i<13;i++) printf("%lf , %lf\n", v_s((double)i), v_t((double)i));
+	return 0;
+}
