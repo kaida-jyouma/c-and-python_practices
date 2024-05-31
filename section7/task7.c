0e8957a354b54dda88a361b72eb2293f708a8840
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-05-31 09:37:10 +0900

    update task7.c

--- section7/task7.c
+++ section7/task7.c
@@ -22,20 +22,49 @@ int main(){
 // 7task3.c
 
 /*
-int checkSign4(int a, int b, int c, int d){
-	int ct = 0;
-	int ls[4] = {a, b, c, d};
-	for (int i=0;i<3;i++){
-		for (int j=0;j<(3-i);j++){
-			if (ls[i] > ls[i+j+1]){
-				ct += 1;
-			}
-		}
-	}
-	if (ct % 2 == 0){
-		return 1;
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// 7task3.c
+
+
+int checkSign4(int lnum){
+	// old code
+	// int ct = 0;
+	// int ls[4] = {a, b, c, d};
+	// for (int i=0;i<3;i++){
+	// 	for (int j=0;j<(3-i);j++){
+	// 		if (ls[i] > ls[i+j+1]){
+	// 			ct += 1;
+	// 		}
+	// 	}
+	// }
+	// if (ct % 2 == 0){
+	// 	return 1;
+	// }else{
+	// 	return -1;
+	// }
+
+	// unnecessary? -> necessary
+	int u0 = 1;
+	if (lnum < abs(lnum)) u0 = -1;
+	
+	lnum = abs(lnum);
+	
+	int a = (lnum - (lnum % 1000)) / 1000;
+	int b = (lnum - (lnum % 100) - (a * 1000)) / 100;
+	int c = (lnum - (lnum % 10) - (a * 1000 + b * 100)) / 10;
+	int d = lnum % 10;
+	// printf("%d %d %d %d\n", a, b, c, d);
+	if (a > b){
+		return checkSign4((-1) * (u0) * (b * 1000 + a * 100 + c * 10 + d));
+	}else if (b > c){
+		return checkSign4((-1) * (u0) * (a * 1000 + c * 100 + b * 10 + d));
+	}else if (c > d){
+		return checkSign4((-1) * (u0) * (a * 1000 + b * 100 + d * 10 + c));
 	}else{
-		return -1;
+		return (u0 * (a * 1000 + b * 100 + c * 10 + d));
 	}
 }
 
@@ -49,7 +78,20 @@ int main(){
 						continue;
 					}else{
 						printf("%d %d %d %d , ", i+1, j+1, k+1, l+1);
-						printf("%d\n", checkSign4(i, j, k, l));
+						int a, b, c, d;
+						a = (i + 1) * 1000;
+						b = (j + 1) * 100;
+						c = (k + 1) * 10;
+						d = (l + 1);
+						// printf("%d , ", a + b + c + d);
+						int lnum = checkSign4(a + b + c + d);
+						
+						if (lnum < 0){
+							printf("%d\n", -1);
+						}else{
+							printf("%d\n", 1);
+						}
+						
 					}
 				}
 			}
