a3714f82ea70cee4836b94f8359dc480e2175ad7
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-06-12 22:02:36 +0900

    complete task1 in task9.c

--- section9/task9.c
+++ section9/task9.c
@@ -16,6 +16,39 @@ int gainedScore(int i, int u1, int u2, int u3){
 	}
 }
 
+double mean(int ls[], int length){
+	double total = 0;
+	for (int i=0;i<length;i++){
+		total += ls[i];
+	}
+	return total / (double)length;
+}
+
+double variance(int ls[], int length){
+	double total = 0;
+	double total2 = 0;
+	for (int i=0;i<length;i++){
+		total += ls[i];
+		total2 += pow(ls[i], 2);
+	}
+	return (total2 / (double)length) - pow(total / (double)length, 2.0);
+}
+
+double cov(int ls1[], int ls2[], int length){
+	double m1 = mean(ls1, length);
+	double m2 = mean(ls2, length);
+	double multi = 0;
+	for (int i=0;i<length;i++) multi += ls1[i] * ls2[i];
+	return multi / (double)length - m1 * m2;
+
+}
+
+int failed(int ls[], int length){
+	int ct = 0;
+	for (int i=0;i<length;i++) if (ls[i] < 60) ct ++;
+	return ct;
+}
+
 int main(){
 	int scoreLists[SUBJECTS][STUDENTS] = {0};
 	int coeff[SUBJECTS][3] = {
@@ -25,16 +58,34 @@ int main(){
 		{10000, 20000, 40000}
 	};
 	for (int i=0;i<SUBJECTS;i++){
-		for (int j=0;j<STUDENTS;j++){
-			scoreLists[i][j] = gainedScore(j, coeff[i][0], coeff[i][1], coeff[i][2], coeff[i][3]);
+		for (int j=0;j<STUDENTS / 2;j++){
+			scoreLists[i][j] = gainedScore(j, coeff[i][0], coeff[i][1], coeff[i][2]);
 		}
 	}
-	printf("index: math, engl, phys, prog");
-	for (int i=0;i<STUDENTS;i++){
-		printf("%d: %d, %d, %d\n", scoreLists[0][i], scoreLists[1][i], scoreLists[2][i], scoreLists[3][i]);
+	printf("index: mathp, engl, phys, prog\n");
+	for (int i=0;i<STUDENTS / 2;i++){
+		printf("%d: %d, %d, %d, %d\n", i+1, scoreLists[0][i], scoreLists[1][i], scoreLists[2][i], scoreLists[3][i]);
 	}
-}
 
+	printf("ave, variance, failure\n");
+	for (int i=0;i<SUBJECTS;i++){
+		printf("%lf, %lf, %d\n", mean(scoreLists[i], STUDENTS / 2), variance(scoreLists[i], STUDENTS / 2), failed(scoreLists[i], STUDENTS / 2));
+	}
+
+	printf("\ncovariance\n");
+	printf("math and engl: ");
+	printf("%lf\n", cov(scoreLists[0], scoreLists[1], STUDENTS / 2));
+	printf("math and phys: ");
+	printf("%lf\n", cov(scoreLists[0], scoreLists[2], STUDENTS / 2));
+	printf("math and prog: ");
+	printf("%lf\n", cov(scoreLists[0], scoreLists[3], STUDENTS / 2));
+	printf("engl and phys: ");
+	printf("%lf\n", cov(scoreLists[1], scoreLists[2], STUDENTS / 2));
+	printf("engl and prog: ");
+	printf("%lf\n", cov(scoreLists[1], scoreLists[3], STUDENTS / 2));
+	printf("phys and prog: ");
+	printf("%lf\n", cov(scoreLists[2], scoreLists[3], STUDENTS / 2));
+}
 
 
 // 9task22 ..... Under Construcion -> Complete
