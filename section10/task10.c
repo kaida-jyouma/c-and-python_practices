1b08e3161883bedae976dd8b75bd99245d733b78
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-06-25 14:27:53 +0900

    edit task10.c

--- section10/task10.c
+++ section10/task10.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
+#include <stdlib.h>
+
+void sort(int ls[], int length){
+    int *default;
+    default = calloc(length, sizeof(int))
+}
+
 
 int main(){
 	
